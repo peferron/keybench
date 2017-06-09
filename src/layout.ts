92fcdd8689e1ee6b0e07612f663f2f91f5843505
Pierre-Emile Ferron <peferron@gmail.com>
2017-06-09 01:33:51 -0700

    Implement layout parsing

---
+++ src/layout.ts
@@ -0,0 +1,27 @@
+import {inspect} from 'util';
+
+interface CharacterInfo {
+    modifiers: string[];
+}
+
+interface LayoutEntry extends CharacterInfo {
+    characters: string;
+}
+
+export default function parse(json: string): Map<string, CharacterInfo> {
+    const layout: LayoutEntry[] = JSON.parse(json);
+    const map: Map<string, CharacterInfo> = new Map();
+
+    for (const {characters, ...info} of layout) {
+        for (const char of characters) {
+            if (map.has(char)) {
+                throw new Error(`Character "${char}" should be present only once per layout, but ` +
+                    `is present both with ${inspect(map.get(char))} and with ${inspect(info)}.`);
+            }
+
+            map.set(char, info);
+        }
+    }
+
+    return map;
+}
---
+++ tests/test_layout.ts
@@ -0,0 +1,20 @@
+import * as fs from 'fs';
+import * as assert from 'assert';
+
+import parseLayout from '../src/layout';
+
+const json = fs.readFileSync('tests/inputs/layout.json', 'utf8');
+const layout = parseLayout(json);
+
+// Test symbols that are missing from the layout.
+assert.strictEqual(layout.get('µ'), undefined);
+
+// Test modifiers.
+assert.deepStrictEqual(layout.get('a'), {"modifiers": []});
+assert.deepStrictEqual(layout.get('A'), {"modifiers": ['shift']});
+
+// Test tricky characters.
+assert.deepStrictEqual(layout.get('`'), {"modifiers": []});
+assert.deepStrictEqual(layout.get('\\'), {"modifiers": []});
+assert.deepStrictEqual(layout.get('\''), {"modifiers": []});
+assert.deepStrictEqual(layout.get('"'), {"modifiers": ['shift']});
