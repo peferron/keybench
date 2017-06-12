e86977136a94a8d75711b2b72e375396f2520579
Pierre-Emile Ferron <peferron@gmail.com>
2017-06-12 03:23:54 -0700

    Remove temp var

--- src/builtin.ts
+++ src/builtin.ts
@@ -4,6 +4,5 @@ import * as fs from 'fs';
 export default function builtInLayoutPaths(): string[] {
     const dir = path.join(__dirname, '../layouts');
     const names = fs.readdirSync(dir);
-    const paths = names.map(name => path.join(dir, name));
-    return paths;
+    return names.map(name => path.join(dir, name));
 }
